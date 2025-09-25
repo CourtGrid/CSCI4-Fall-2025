569763c54c4a77a391d1c0dfc7c16b7f157c4d93
CourtGrid <cgridley001@student.butte.edu>
2025-09-25 17:02:26 +0000

    commit

--- Hello World Assignment/hello_world_notes.py
+++ Hello World Assignment/hello_world_notes.py
@@ -10,7 +10,8 @@
 #Just tried it out, it did exactly what it was supposed to do, which is print ‘abc’ out as [‘a’, ‘b’, ‘c’]
 #Wow Wait just realized that when I looked at the range list examples that when you put down ‘10’, it actually only lists up to 9 which is fun.
 
-#Wait, I think I understand how this works. I THINK it starts counting at 0, but the computer is just treating the ‘0’ like we would a 1, so once it gets up to 9 the computer has counted 10 integers. I’m going to test stuff out. 
+#Wait, I think I understand how this works. I THINK it starts counting at 0, but the computer is just treating the ‘0’ like we would a 1, 
+#so once it gets up to 9 the computer has counted 10 integers. I’m going to test stuff out. 
 
 #Just realized I can attach any name I want to the list. 
 
@@ -18,7 +19,9 @@
 
 #Just now realizing range was part of something else…oops :>
 
-#Saw the part under lists about sorting, and saw that they had a bit of a tutorial so I figured I would try it out. I found that when I want to sort out a list numerically, all I have to do is type out list.sort(). This is (I’m pretty sure) just because there aren’t any specifications that I added in, so it automatically lists it numerically. I don’t know if it does the same alphabetically. I’m going to test that.
+#Saw the part under lists about sorting, and saw that they had a bit of a tutorial so I figured I would try it out. I found that when I want to sort out a list numerically, 
+#all I have to do is type out list.sort(). This is (I’m pretty sure) just because there aren’t any specifications that I added in, so it automatically lists it numerically. 
+#I don’t know if it does the same alphabetically. I’m going to test that.
 
 #It DOES list it out alphabetically :D
 
@@ -32,4 +35,20 @@
 
 #Lists are sequences used to store collections of homogeneous items.
 
-#An array is similar to a list, but the type of objects that are stored in them is constrained
+#An array is similar to a list, but the type of objects that are stored in them is constrained.
+#An array stores a bunch of similar objects in a structured format.
+
+#Arrays and Lists are different to collections because they are structured/organized in a more particular manner than a collection is.
+
+#A collection is a group of organized objects, or variables, or nothing, that are organized in some manner. 
+#A collection doesn't have to organized in a meaningful way, it can be done randomly.
+
+#Note to self: dissect the frog
+
+#Arrays allow you to structure data of any type, allow for portability, and allow for creation of functions. 
+#This is me making an assumption, but I believe that you create the functions so that data can be transfferred between two different programs or programming languages? Maybe?
+
+#Functions outputs cannot be ambiguous. (fancy f hehe :3)
+#Functions create mappings between two sets. 
+#Functions only take particular types of things. The object type is specific. 
+#Functions have an input and an output. 
