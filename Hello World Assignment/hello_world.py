1c241b539a890b2fa3b7120551e38ed5cbc2edf3
CourtGrid <cgridley001@student.butte.edu>
2025-09-25 17:25:37 +0000

    commit

--- Hello World Assignment/hello_world.py
+++ Hello World Assignment/hello_world.py
@@ -1,5 +1,6 @@
-# wow :O
+# wow :3
 
+#Hello World:
 hello = "world"
 
 this_number = "42"
@@ -10,10 +11,12 @@ this_number + 1
 
 print(this_number, 5)
 
+#List:
 hi=list(range(1,11))
 
 print(hi)
 
+#Array
 import array as boop
 
 a = boop.array('l', [1, 2, 3, 4, 5])
@@ -23,4 +26,10 @@ a = boop.array('l', [1, 2, 3, 4, 5])
 print(a.pop())
 #array doesn't have a proper way to print
 #pop will only show the LAST thing in the list
+
+#Function
+# f : Num-> Num
+def f(x): #define a function using "def" <name>(<input list)
+    return (x + 1) #must return some value
+print(f(2), f(4), f(6))
 
