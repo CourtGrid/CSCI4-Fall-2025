9dbc717702eeaccfe2e0b19a32a1a5a0b6f777c0
CourtGrid <cgridley001@student.butte.edu>
2025-10-23 18:41:51 +0000

    commit

--- Hi :D/hi.py
+++ Hi :D/hi.py
@@ -1,6 +1,10 @@
 def main(x,y,z):
     print(x,y,z)
 
-l=[1,2,3,4,5,5]
-i=[0,3,4,3,4,5]
-main("hello", l[i[5]], l[i[2]])
+l=[1,2,3,4,5]
+i=[0,1,2,3,4]
+main("hello", l[i[4]], l[i[3]])
+# l[i[4]] tells us the value of the list l at i index 4. 
+# In this scenario (unless i changed it) the value is 5
+# If you change the values of i, the output of main will
+# change in some weird way that I still don't fully understand.
--- midterm/experimenting.py
+++ midterm/experimenting.py
@@ -1,3 +1,7 @@
 def farey(named_variable):
-    f = [0, 1]
+    farey_sequence = [(0,1), (0,.5,1)]
+    o=1
+
+
+
 
